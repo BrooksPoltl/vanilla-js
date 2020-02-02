b6221c08becd9696967aa5c29b3a565dc8118f3f
BrooksPoltl <bpoltl1@gmail.com>
2020-02-02 11:14:51 -0600

    reduce

--- dom-array-methods/script.js
+++ dom-array-methods/script.js
@@ -24,6 +24,15 @@ async function getRandomUser() {
   addData(newUser);
 }
 
+function calculateWealth() {
+  const wealth = data.reduce((acc, user) => (acc += user.money), 0);
+  const wealthEl = document.createElement('div');
+  wealthEl.innerHTML = `<h3>Total Wealth: <strong>${formatMoney(
+    wealth
+  )}</strong></h3>`;
+  main.appendChild(wealthEl);
+}
+
 function showMillionaires() {
   data = data.filter(user => user.money >= 1000000);
   updateDOM();
@@ -67,3 +76,4 @@ addUserBtn.addEventListener('click', getRandomUser);
 doubleBtn.addEventListener('click', doubleMoney);
 sortBtn.addEventListener('click', sortByRichest);
 showMillionairesBtn.addEventListener('click', showMillionaires);
+calculateWealthBtn.addEventListener('click', calculateWealth);
